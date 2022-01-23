78cbb7134db89a75b7db3f05a53f486945c7bf42
senateurritchie <zakeszako@yahoo.fr>
2022-01-23 01:54:38 +0000

    move coa:maintenance command to it bundle v6

--- src/Command/CoaMaintenanceCommand.php
+++ src/Command/CoaMaintenanceCommand.php
@@ -1,5 +1,5 @@
 <?php
-namespace Coa\MaintenanceBundle;
+namespace Coa\MaintenanceBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
