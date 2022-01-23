6c38acf11c533a147b0e987541d732bf31602773
senateurritchie <zakeszako@yahoo.fr>
2022-01-23 01:29:24 +0000

    move coa:maintenance command to it bundle

---
+++ src/Command/MaintenanceCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Coa\MaintenanceBundle;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class MaintenanceCommand extends Command
+{
+    protected static $defaultName = 'coa:maintenance';
+    protected static $defaultDescription = 'Mets le site en mode maintenance';
+    private ContainerInterface $container;
+
+    public function __construct(string $name = null,ContainerInterface $container)
+    {
+        parent::__construct($name);
+        $this->container = $container;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addOption('status', null, InputOption::VALUE_REQUIRED, 'pour activer ou desactive le mode maintenance')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $mode = filter_var($input->getOption('status'),FILTER_VALIDATE_BOOLEAN);
+        $io->title("Mode maintenance");
+
+        // activation du mode maintenance
+        $filename = $this->container->getParameter('kernel.project_dir') . "/.maintenance";
+        if($mode == true){
+            $io->note("Activation mode maintenance");
+            if(!file_exists($filename)){
+                file_put_contents($filename,"on");
+                $io->success("Mode maintenance activé");
+            }
+            else{
+                $io->error("Mode maintenance déja activé");
+            }
+        }
+        else{
+            $io->note("Desactivation mode maintenance");
+            if(file_exists($filename)){
+                @unlink($filename);
+                $io->success("Mode maintenance désactivé");
+            }
+            else{
+                $io->error("Mode maintenance déja désactivé");
+            }
+        }
+        return Command::SUCCESS;
+    }
+}
